a516fce818bad6910bc6d79892381ea9a37f1d62
thejohnphilip <thejohnphilipv@gmail.com>
2024-07-11 17:46:26 -0400

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/github-pages',
+  basePath: '/portfolio', 
+  assetPrefix: '/portfolio',
+  images: {
+    unoptimized: true, // This is important for Next.js static export
+  },
 };
+
 
 export default nextConfig;
