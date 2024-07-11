b147b90bb41759b1877ce64aee54e1f6190821c6
thejohnphilip <thejohnphilipv@gmail.com>
2024-07-11 19:14:32 -0400

    delete github workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/github-pages',
-  basePath: '/portfolio', 
-  assetPrefix: '/portfolio',
-  images: {
-    unoptimized: true, // This is important for Next.js static export
-  },
+  
 };
-
 
 export default nextConfig;
